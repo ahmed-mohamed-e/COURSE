ae6e9a71a231b4be4ab95111f6daa20cecf15b5f
ahmed-mohamed-e <06zer0day06@gmail.com>
2025-03-02 21:53:13 +0200

    Add files via upload

---
+++ pizza_order.py
@@ -0,0 +1,29 @@
+database = {"S":15,"M":20,"L":25}
+total = 0
+while True:
+    size = input("Enter your choice: ").upper()
+    if size in database.keys():
+        total += database[size]
+        break
+    else:
+        print("Enter a valid choice...")
+if size == "S":
+    add_pepperoni = input("Do you wanna add pepperoni (Y or N): ").upper()
+    if add_pepperoni == "Y":
+        total += 2
+    else:
+        pass
+elif size == "M":
+    add_pepperoni = input("Do you wanna add pepperoni (Y or N): ").upper()
+    if add_pepperoni == "Y":
+        total += 3
+    else:
+        pass
+else:
+    pass
+extra_cheese = input("Do you wanna add extra cheese (Y or N): ").upper()
+if extra_cheese == "Y":
+    total += 1
+else:
+    pass
+print(f"Total is ${total}")
